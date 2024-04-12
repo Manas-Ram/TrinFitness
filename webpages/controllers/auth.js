7301b7346f1d5f9fc177ec220a0471ff055c503a
Manas-Ram <68700139+Manas-Ram@users.noreply.github.com>
2024-04-12 15:31:26 -0400

    auth

---
+++ webpages/controllers/auth.js
@@ -0,0 +1,6 @@
+const express = require('express'),
+  router = express.Router();
+const session = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const KEYS = require('../config/keys.json');

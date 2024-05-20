d5b397dfe83d1e76ee82d0b27634a1a03f28712c
Manas-Ram <68700139+Manas-Ram@users.noreply.github.com>
2024-05-20 01:10:41 -0400

    Update keys.js

--- webpages/config/keys.js
+++ webpages/config/keys.js
@@ -1,7 +1,7 @@
 
-require('dotenv').config();
+
 module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    sessionSecret: process.env.SESSION_SECRET
+    googleClientID: process.env.GOOGLE_CLIENT_ID, //replace with the id provided by Mr. Gohde
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET, //reaplce with the google client secret provided by Mr. Gohde
+    sessionSecret: process.env.SESSION_SECRET //replace with the secret provided in the README file
 };

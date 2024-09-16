73471d34452c594a5c999aca10362312c6cfe39e
Manas-Ram <68700139+Manas-Ram@users.noreply.github.com>
2024-09-16 12:55:53 -0400

    Update keys.js

--- webpages/config/keys.js
+++ webpages/config/keys.js
@@ -1,7 +1,7 @@
 
 
 module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID, //replace with the id provided by Mr. Gohde
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET, //reaplce with the google client secret provided by Mr. Gohde
+    googleClientID: process.env.GOOGLE_CLIENT_ID, //replace with the id provided by your credentials
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET, //reaplce with the google client secret provided by your credentials
     sessionSecret: process.env.SESSION_SECRET //replace with the secret provided in the README file
 };
